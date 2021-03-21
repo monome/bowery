a768350d8f53d5888b29aba6522f80394aefff3d
trentgill <trent.gill@gmail.com>
2021-03-22 03:51:39 +0700

    basic.lua

--- basic.lua
+++ basic.lua
@@ -1,10 +1,9 @@
 --- basic.lua
 
 -- public variables
--- public.add('time', 1.1)
 public.add('time', 1.1, {0.1,10}, function(v) metro[1].time = v end)
 public.add('seq', {1,2,3,4}) -- create a table set
-public.seq.select(1) -- enter the table
+public.seq.select(1) -- seq table has a pointer
 public.add('level',0,{-5,5,'slider'}, function(v) output[1].volts = v end)
 public.add('a',32)
 public.add('b',1111)
@@ -20,4 +19,5 @@ function sh()
   public.level = public.seq.step() -- advance sequence on each step
 end
 
+--- future norns update will append preset values literally like below
 -- public.time = 3.2
