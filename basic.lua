c1779084294c5808c38c9a033b4dc4a6e9a4fc70
trentgill <trent.gill@gmail.com>
2021-03-21 09:02:02 +0700

    working implementation of public table params

--- basic.lua
+++ basic.lua
@@ -1,10 +1,29 @@
 --- basic.lua
--- START MODULE
-
 --- params library -- RENAME PUBLIC
-Params = {} -- this 
+Params = {} -- FIXME make this local
 Params._names = {} -- keys are names, linked to indexed _params
 Params._params = {} -- storage of params
+
+Params.Pubtable = {} -- metamethods for methods on a tables in the public variable space
+Params.Pubtable.__newindex = function(self, ix, val)
+  print "pubtable newindex"
+end
+Params.Pubtable.__index = function(self, ix) -- getters
+    if ix == 'index' then
+        return function(n)
+            if not n then print "no index provided"; return end
+            self._index = ((n-1) % #self) + 1 -- wrap to table size
+            return self[self._index]
+          end
+    elseif ix == 'step' then
+        return function(n)
+            self._step = n or self._step
+            self._index = (((self._index + self._step)-1) % #self) + 1 -- wrap to table size
+            return self[self._index]
+          end
+    end
+end
+setmetatable(Params.Pubtable, Params.Pubtable)
 
 Params.add = function(name, default, typ, action)
     local ix = Params._names[name]
@@ -12,7 +31,22 @@ Params.add = function(name, default, typ, action)
         ix = #(Params._params) + 1
         Params._names[name] = ix
     end
-    Params._params[ix] = { k=name, v=default or 0 }
+    if type(default) == 'table' then
+      -- TODO this should be Pubtable.new()
+        print("discover#: "..#Params._params)
+        Params._params[ix] = default -- WARNING overwrite above, so table is flat
+          print("discover#: "..#Params._params)
+        local p = Params._params[ix] -- alias
+        p.k = name
+        p.v = p -- self-reference
+        p._index = 0 -- invalid index
+        p._step = 1 -- default step motion
+        setmetatable(p, Params.Pubtable) -- overload the parameter with table semantics
+                  print("discover#: "..#Params._params)
+    else
+        Params._params[ix] = { k=name, v=default or 0 }
+    end
+    -- FIXME handle type restriction on table type
     if typ then -- type restriction applies
         local p = Params._params[ix] -- alias
         -- TODO add 'action' handling - type match on function
@@ -55,18 +89,27 @@ Params.clear = function()
   Params._params = {}
 end
 
-
+-- FIXME refactor this please!
 Params.discover = function()
-  -- FIXME send type data in a table
   for _,p in ipairs(Params._params) do
       -- name
       local s = string.format('pub(%q,',p.k)
       -- value
-      if type(p.v) == 'string' then
-        s = string.format('%s%q,{',s,p.v)
-      else
-        s = s .. p.v .. ',{'
+      local tv = type(p.v)
+      local def = p.v
+      if tv == 'string' then
+        def = string.format('%q',p.v)
+      elseif tv == 'table' then
+        -- { vals }
+        def = '{'
+        for k,v in ipairs(p.v) do
+          def = def .. v .. ','
+        end
+        -- TODO append .index=p._index
+        def = def .. 'index=' .. p._index .. '}'
       end
+      s = s .. def .. ',{'
+      
       -- type
       if p.enum then
         s = string.format('%s%q,', s, 'enum')
@@ -79,6 +122,7 @@ Params.discover = function()
         if p.type then s = string.format('%s%q', s, p.type) end
       end
       s = s .. '})'
+      print(s)
       print('^^'..s) -- send to remote
   end
   _c.tell('pub',string.format('%q','_end'))
@@ -128,26 +172,21 @@ setmetatable(Params, Params)
 -- END MODULE
 public = Params
 
-
---- basic sample & hold
--- in1: sampling clock
--- out1: random sample, updated on each clock pulse
-
 -- public variables
-public.add('range', 3, {0,10})
 public.add('time', 1.1, {0.1,10}, function(v) metro[1].time = v end)
--- public.add('scale', 'minor', {'minor','major','penta'})
-public.add('volts', 0, {-5,5,'slider'})
+public.add('seq', {1,2,3,4}) -- create a table set
+-- public.add('seq', {1,2,3,4,5,6,7,8}) -- create a table set
+public.seq.index(1) -- make that set an indexed table
+-- TODO change syntax to: public.seq[i]
 
 function init()
-  input[1].change = sh
-  input[1].mode('change',1,0.1,'rising')
-  metro[1].time = 3
+  metro[1].time = 1
   metro[1].event = sh
   metro[1]:start()
 end
 
 function sh()
-  public.volts = (math.random() - 0.5) * public.range
-  output[1].volts = public.volts
+  -- public.seq.step() -- advance sequence on each step
 end
+
+public.time = 3.2
