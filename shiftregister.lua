c29e0b5e2c0278a8354219c74a7d5c79ba50be46
Trent Gill <trent.gill@gmail.com>
2021-05-12 23:50:22 +0700

    update shiftreg

--- shiftregister.lua
+++ shiftregister.lua
@@ -6,10 +6,10 @@
 -- ii: 6 stages of ASR via just friends
 
 -- public params
-public.add('portamento', 0, {0, 0.5}, function(v) for n=1,4 do output[n].slew = v end end)
-public.add('ii_dest', 'none', {'none','jf','wsyn'}
-    , function(e) if e=='jf' then ii.jf.mode(1) end end) -- enable synthesis mode
-public.add('ii_velo', 2, {0.1, 5})
+public{slew = 0.001}:xrange(0.001, 0.5):action(function(v) for n=1,4 do output[n].slew = v end end)
+public{ii_dest = 'none'}:options{'none','jf','wsyn'}
+    :action(function(e) if e=='jf' then ii.jf.mode(1) end end) -- enable synthesis mode
+public{ii_velo = 2}:range(0.1, 5)
 
 -- global state
 reg = {0,0,0,0,0,0}
@@ -21,7 +21,7 @@ function make_notes(r)
   if public.ii_dest ~= 'none' then
     local count = (public.ii_dest == 'jf') and 6 or 4
     for n=1,count do
-      ii[public.ii_dest]play_note(r[n], public.ii_velo)
+      ii[public.ii_dest].play_note(r[n], public.ii_velo)
     end
   end
 end
